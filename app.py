979a65ba5d6946d0c2b134009c1b830e80aa4f22
JUAN DAVID <sacul010393@gmail.com>
2026-09-16 19:59:00 -0400

    commit title .app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Anuncio de venta de vehículos en Estados Unidos')
+st.header('Anuncio de ventas de vehículos en Estados Unidos')
 
 hist_button = st.checkbox('Construir histograma')
 
